931e50e09f97138986868869e1842ad92c5aa1bc
jassim mohamed <159074105+jasbx@users.noreply.github.com>
2024-06-14 19:23:41 +0300

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const resolvers =require('./GraphQl/resolvers')
 
 //connect with mongo db
 const   mongoose =require ('mongoose');
-mongoose.connect('mongodb+srv://gloop216:t6OwXxDJu1KR9YA3@cluster0.nryicsp.mongodb.net/GraphQl?retryWrites=true&w=majority&appName=Cluster0').then(()=>console.log('connecting ok !'))
+mongoose.connect('MONGODB URL HERE').then(()=>console.log('connecting ok !'))
 const server = new ApolloServer({
     typeDefs,
   resolvers,
